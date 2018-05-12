da6c18ffda4ddd44b96a7d2aa070000256e14609
mekows <1361394078@qq.com>
2018-05-12 20:02:47 +0800

    commit  first

---
+++ src/main/java/com/example/mekows/MekowsApplication.java
@@ -0,0 +1,12 @@
+package com.example.mekows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MekowsApplication.class, args);
+    }
+}
